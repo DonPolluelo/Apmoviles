763f5091de89d90d4a3a66a1a22bb24d0b24ddd7
Kenji Tadashi Tsutsumi Rebolledo <DonPolluelo@localhost.localdomain>
2018-10-19 13:12:31 -0300

    Proyecto

---
+++ UniUp/app/src/main/java/com/example/uniup/uniup/HorarioFragment.java
@@ -0,0 +1,28 @@
+package com.example.uniup.uniup;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HorarioFragment extends Fragment {
+
+    public HorarioFragment() {
+    }
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.horario_fragment, container, false);
+        return view;
+    }
+
+
+
+
+}
---
+++ UniUp/app/src/main/java/com/example/uniup/uniup/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.uniup.uniup;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.uniup.uniup.HorarioFragment;
+import com.example.uniup.uniup.LinksFragment;
+import com.example.uniup.uniup.MallaFragment;
+import com.example.uniup.uniup.R;
+import com.example.uniup.uniup.SemestreFragment;
+import com.example.uniup.uniup.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewpager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new HorarioFragment(), "Horario");
+        adapter.AddFragment(new MallaFragment(), "Malla");
+        adapter.AddFragment(new SemestreFragment(), "Semestre");
+        adapter.AddFragment(new LinksFragment(), "Links");
+
+        viewpager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewpager);
+
+    }
+}
