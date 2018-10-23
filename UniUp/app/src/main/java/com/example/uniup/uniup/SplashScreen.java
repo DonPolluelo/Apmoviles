77add6f343ece272867499d494f658e4e47fd5ab
Kenji Tadashi Tsutsumi Rebolledo <DonPolluelo@localhost.localdomain>
2018-10-23 02:59:12 -0300

    Se agrega imagen al comienzo

---
+++ UniUp/app/src/main/java/com/example/uniup/uniup/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.uniup.uniup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_inicio);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, MenuUniversidadActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 4000);
+    }
+
+
+}
